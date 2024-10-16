dcb67d33020eafbc7d176c01f11e859ef9d01a98
MrSnoodle01 <richardsonjosh03@gmail.com>
2024-10-15 20:27:39 -0700

    added basic navbar

--- Tabby/components/FooterNavBar.tsx
+++ Tabby/components/FooterNavBar.tsx
@@ -1,15 +1,34 @@
-import { View } from "react-native";
+import { View, Image } from "react-native";
 import { Link } from "expo-router";
 import React from "react";
 
 const FooterNavBar = () => {
+  return (
+    <View className="bottom-0 flex-row h-16 space-x-8 bg-zinc-700">
+      <Link href="/Library" className="text-white ">
+        {" "}
+        <Image
+          className="self-center w-10 h-10"
+          source={require("@/assets/navbar-images/notSelectedLibrary.png")}
+        />
+      </Link>
+      <Link href="/recommendations" className="text-white">
+        {" "}
+        <Image
+          className="self-center w-10 h-10"
+          source={require("@/assets/navbar-images/notSelectedFavorite.png")}
+        />
+      </Link>
+      <Image
+        className="w-16 h-16"
+        source={require("@/assets/navbar-images/camera.png")}
+      />
+      <Image
+        className="absolute right-0 self-center w-10 h-10"
+        source={require("@/assets/navbar-images/settings.png")}
+      />
+    </View>
+  );
+};
 
-    return (
-        <View className=" flex-1 flex-row items-end">
-            <Link href="/library" className="p-5 text-white"> Library</Link>
-            <Link href="/recommendations" className="p-5 text-white"> Reccomendations</Link>
-        </View>
-    )
-}
-
-export default FooterNavBar
+export default FooterNavBar;
