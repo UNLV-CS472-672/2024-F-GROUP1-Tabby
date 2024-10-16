a3b2f784be621e4a6b44368246ea7bde77cc349d
MrSnoodle01 <richardsonjosh03@gmail.com>
2024-10-16 11:53:12 -0700

    styled the navbar to look like the figma

--- Tabby/components/FooterNavBar.tsx
+++ Tabby/components/FooterNavBar.tsx
@@ -1,32 +1,52 @@
-import { View, Image } from "react-native";
-import { Link } from "expo-router";
+import { View, Image, Text } from "react-native";
+import { Link, usePathname } from "expo-router";
 import React from "react";
 
 const FooterNavBar = () => {
+  const pathname = usePathname();
   return (
-    <View className="bottom-0 flex-row h-16 space-x-8 bg-zinc-700">
-      <Link href="/Library" className="text-white ">
-        {" "}
+    <View className="absolute justify-center bottom-0 flex-row items-start h-16 w-screen bg-[#1d232b]">
+      <View className="absolute left-0 flex-col">
+        <Link href="/Library">
+          {" "}
+          <Image
+            className="w-10 h-10"
+            source={
+              pathname === "/Library"
+                ? require("@/assets/navbar-images/selectedLibrary.png")
+                : require("@/assets/navbar-images/notSelectedLibrary.png")
+            }
+          />
+        </Link>
+        <Text className="text-white">Library</Text>
+      </View>
+      <View className="absolute flex-col left-16">
+        <Link href="/recommendations">
+          {" "}
+          <Image
+            className="w-10 h-10"
+            source={
+              pathname === "/recommendations"
+                ? require("@/assets/navbar-images/selectedFavorite.png")
+                : require("@/assets/navbar-images/notSelectedFavorite.png")
+            }
+          />
+        </Link>
+        <Text className="text-white">Favorites</Text>
+      </View>
+      <View className="w-16 h-16 bg-white rounded-full -top-8">
         <Image
-          className="self-center w-10 h-10"
-          source={require("@/assets/navbar-images/notSelectedLibrary.png")}
+          className="w-16 h-16"
+          source={require("@/assets/navbar-images/camera.png")}
         />
-      </Link>
-      <Link href="/recommendations" className="text-white">
-        {" "}
+      </View>
+      <View className="absolute right-0 flex-col px-4">
         <Image
-          className="self-center w-10 h-10"
-          source={require("@/assets/navbar-images/notSelectedFavorite.png")}
+          className="self-start w-10 h-10"
+          source={require("@/assets/navbar-images/settings.png")}
         />
-      </Link>
-      <Image
-        className="w-16 h-16"
-        source={require("@/assets/navbar-images/camera.png")}
-      />
-      <Image
-        className="absolute right-0 self-center w-10 h-10"
-        source={require("@/assets/navbar-images/settings.png")}
-      />
+        <Text className="bottom-0 text-white">Menu</Text>
+      </View>
     </View>
   );
 };
