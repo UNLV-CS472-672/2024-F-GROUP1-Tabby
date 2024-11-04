c3f7f1a52064ce7f17e3549072fa86663b05abcf
LilAttacker <danielepratt0@gmail.com>
2024-11-04 14:25:55 -0800

    Updates tests
    Changes test to yolo

--- server/tests/test_api.py
+++ server/tests/test_api.py
@@ -123,14 +123,14 @@ class TestAPIEndPoint:
     def test_predict_examples(self, client):
         # Calls for the model to run object detection on example images.
         # Does not save the results.
-        result = client.get("/test/shelf_read", query_string={"index": int(0)})
+        result = client.get("/yolo/shelf_read", query_string={"index": int(0)})
 
         # This should always return true.
         assert result.status_code == HTTPStatus.OK
         assert result.json is not None and "Detected" in result.json
 
         # Calls the model but provides an incorrect index
-        result = client.get("/test/shelf_read", query_string={"index": int(2)})
+        result = client.get("/yolo/shelf_read", query_string={"index": int(2)})
 
         # This should always return false.
         assert result.status_code == HTTPStatus.BAD_REQUEST
