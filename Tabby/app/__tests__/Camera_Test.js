9400f4dac80031e018ce4b703a01da4e6a476ded
Josh <richardsonjosh03@gmail.com>
2024-11-08 15:45:44 -0800

    test for if camera modal opens when prompted

--- Tabby/app/__tests__/Camera_Test.js
+++ Tabby/app/__tests__/Camera_Test.js
@@ -1,21 +1,31 @@
-// import React from 'react';
-// import { usePathname } from "expo-router";
-// import FooterNavBar from '@/components/FooterNavBar';
-// import { render, fireEvent, act } from '@testing-library/react-native';
+import React from 'react';
+import { usePathname } from "expo-router";
+import FooterNavBar from '@/components/FooterNavBar';
+import { render, fireEvent, act, screen } from '@testing-library/react-native';
 
-// jest.mock('expo-router', () => ({
-//     usePathname: jest.fn(() => '/app'),
-//     Link: ({ children }) => <>{children}</>,
-// }));
+// when opening modal 5000ms is not enough time to open when testing
+// so doubling it here gives enough time
+jest.setTimeout(10000);
 
-// test('Camera model opens', () => {
-//     const page = render(<FooterNavBar />);
-//     const cameraButton = page.getByTestId('CameraButton');
+jest.mock('expo-router', () => ({
+    usePathname: jest.fn(() => '/library'),
+    Link: ({ children }) => <>{children}</>,
+}));
 
-//     act(() => {
-//         fireEvent.press(cameraButton);
-//     });
+// check to see if Camera modal opens when button is pressed
+test('Camera model opens', async () => {
+    // renders the navbar 
+    const page = render(<FooterNavBar />);
+    // clicks on the camera button
+    const cameraButton = page.getByTestId('CameraButton');
 
-//     expect(usePathname()).toBe('/library');
-// })
+    // opens the modal
+    await act(async () => {
+        fireEvent.press(cameraButton);
+    });
+
+    const modalTest = await screen.findByText('Take Picture');
+
+    expect(modalTest).toBeTruthy();
+})
 
