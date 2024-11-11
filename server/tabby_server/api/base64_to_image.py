e796e2f91f3b20a7241d2a9e989c64e4e7d6c16d
David <lam.david459@yahoo.com>
2024-11-11 15:39:56 -0800

    Update base64 function
    Moved function into api directory
    Function will now output an image matrix instead of creating a file
    Provided two example code to create an image or print the matrix

---
+++ server/tabby_server/api/base64_to_image.py
@@ -0,0 +1,41 @@
+import base64
+import cv2
+import io
+import numpy as np
+
+
+""" Example Useage
+    import tabby_server.api.base64_to_image as base64_to_image
+    from PIL import Image
+
+    img - base64_to_image(base64String)
+    # Print the image matrix
+    print(img)
+
+    # Convert matrix into an image
+    img = Image.fromarray(img, 'RGB')
+
+    # Display image
+    img.show()
+"""
+
+
+def decode_image(string64: str) -> np.ndarray:
+    """ Function will attempt to decript a provided \
+        base 64 string into an image matrix.
+
+    :param string64: Base 64 string. Function will not check to see if it is
+        actually a base 64 string
+    """
+    # First decode the base64 into byte data
+    imgdata = base64.decodebytes(bytes(string64, 'utf-8'))
+
+    # Process the byte data into BGR (Blue, Gree, Red) where
+    # blue occupies the most significant bit and red occupies the LSB
+    img = Image.open(io.BytesIO(imgdata))
+
+    # Don't convert image from BRG into RGB
+    # Just read as is
+    opencv_img = cv2.cvtColor(np.array(img), cv2.IMREAD_COLOR)
+
+    return opencv_img      # Function, END
--- server/tests/test_helper.py
+++ server/tests/test_helper.py
@@ -1,33 +1,14 @@
-import tabby_server.services.helper as helper
+import tabby_server.api.base64_to_image as base64_to_image
 from tests.variable import test_base64
-import os
-import unittest
 
 
-class Helper (unittest.TestCase):
-    def test_image_decoding(self):
-        fName = "tempImage"
-        pPath = "./"
+def test_image_decoding():
+    # Attempt to decript an image and check for it's existance
+    img = base64_to_image.decode_image(test_base64)
 
-        # Error: base64 should be of type string
-        with self.assertRaises(TypeError):
-            helper.image_decoder(64, "FileName Here...")
-
-        # Error: FileName shoudl be of type string
-        with self.assertRaises(TypeError):
-            helper.image_decoder("Base64 string Here...", 44)
-
-        # Error: Path, if specified, must be of type string
-        with self.assertRaises(TypeError):
-            helper.image_decoder("Base64", "File", 66)
-
-        # Attempt to decript an image and check for it's existance
-        helper.image_decoder(test_base64, fName, pPath)
-        assert os.path.isfile(f"{pPath}{fName}.png")
-
-        # Remove the image after the test
-        os.remove(f"{pPath}{fName}.png")
+    # Check the top left pixel to see if it is this exact color
+    assert (img[0, 0] == [208, 192, 169]).all()
 
 
 if __name__ == "__main__":
-    Helper().test_image_decoding()
+    test_image_decoding()
