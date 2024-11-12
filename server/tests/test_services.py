8aa4bb1f0dbc4d1e07dbf8988caffa7cabbab504
LilAttacker <danielepratt0@gmail.com>
2024-11-11 20:28:23 -0800

    Updated Google Books API
    Some minor things had been overlooked or pushed off from original the PR. This rectifies that.
    
    Removes flask dependency from the Google Books API functions so now it can be called more easily from other functions. In its place, I added a flask intermediary function that can be routed to instead for testing purposes.
    
    When returning books from the Google Books API, it now converts the categories/genres into a comma separated string rather than as a list. This was requested by frontend.
    
    Removed resource_format.py. This file is outdated and no longer serves a purpose. I have deleted it.
    
    Updated testing to account for all of this.

--- server/tests/test_services.py
+++ server/tests/test_services.py
@@ -1,3 +1,8 @@
+import json
+import requests_mock
+from http import HTTPStatus
+
+
 def test_get_google_books_query():
     """
     Provides false parameters for a mock query.
@@ -109,7 +114,7 @@ def test_collect_book_attrs():
         "summary": "",
         "thumbnail": "",
         "page_count": "",
-        "categories": "",
+        "genres": "",
         "publisher": "",
         "published_date": "",
     }
@@ -120,11 +125,11 @@ def test_collect_book_attrs():
     fake_book = {
         "industryIdentifiers": [{"identifier": "13", "type": "ISBN_13"}],
         "title": "t",
-        "authors": ["a"],
+        "authors": ["a", "a2", "a3"],
         "averageRating": "r",
         "description": "d",
         "pageCount": "g",
-        "categories": ["c"],
+        "categories": ["c", "c2", "c3"],
         "publisher": "b",
         "publishedDate": "p",
         "imageLinks": {"thumbnail": "n"},
@@ -134,14 +139,109 @@ def test_collect_book_attrs():
     comparison = {
         "isbn": "13",
         "title": "t",
-        "authors": ["a"],
+        "authors": "a,a2,a3",
         "rating": "r",
         "excerpt": "d...",
         "summary": "d",
         "thumbnail": "n",
         "page_count": "g",
-        "categories": ["c"],
+        "genres": "c,c2,c3",
         "publisher": "b",
         "published_date": "p",
     }
     assert result == comparison
+
+
+def test_google_books_search():
+    """
+    Is a direct call to the Google Books Search Function.
+    This will fully test its capability. Should return a dict with book
+    information.
+    """
+
+    # Import function
+    from tabby_server.services.library import google_books_search
+
+    # Load example output.
+    # This was a json output from testing.
+    with open("tests/flower_of_algernon_test_40_output.json", "r") as f:
+        test_output = json.load(f)
+
+    # Setup Mocker
+    # Intercepts all Request Calls.
+    with requests_mock.Mocker() as m:
+        # Intercept an request message and make it always return the
+        # example json.
+        m.get(requests_mock.ANY, json={"book": "bad"})
+
+        # No Arguments Given.
+        # This should fail.
+        result = google_books_search()
+        assert result[0] is not None and "badArgs" in result[0]
+        assert result[1] == HTTPStatus.BAD_REQUEST
+
+        # Correct Arguments, but No Results
+        # The values here don't matter so long as the argument key is
+        # valid. This should pass but we have it see nothing at right now.
+        result = google_books_search(author="fake_author")
+        assert result[0] is not None and "matchless" in result[0]
+        assert result[1] == HTTPStatus.OK
+
+        # Intercepted request messages now return a single blank book.
+        # This is to test if we somehow found books, but none have a
+        # valid ISBN 13 id.
+        m.get(
+            requests_mock.ANY,
+            json={
+                "items": [
+                    {
+                        "industryIdentifiers": [
+                            {"identifier": "123", "type": "bad"},
+                            {"identifier": "456", "type": "bad"},
+                        ]
+                    },
+                    {
+                        "industryIdentifiers": [
+                            {"identifier": "123", "type": "bad"},
+                            {"identifier": "456", "type": "bad"},
+                        ]
+                    },
+                ]
+            },
+        )
+
+        # Matches Found, but None Valid.
+        # Theoretically possible for us to get books in a return but
+        # none of them have an ISBN 13.
+        result = google_books_search(author="fake_author")
+        assert result[0] is not None and "error" in result[0]
+        assert result[1] == HTTPStatus.OK
+
+        # Intercept an request message and make it always return the
+        # example json.
+        # Now all requests will return that full json we have.
+        m.get(requests_mock.ANY, json=test_output)
+
+        # Flowers of Algernon Request.
+        # Again, our values don't actually matter but this is what was
+        # used to get our example anyways.
+        result = google_books_search(phrase="flowers", author="keyes")
+        # The return is actually a json string. So we convert it into
+        # a dict object to work with instead.
+        # This will have all attributes from the Book() dataclass.
+        assert result[1] == HTTPStatus.OK
+        result = json.loads(result[0])
+        assert (
+            result is not None
+            and "isbn" in result
+            and "title" in result
+            and "authors" in result
+            and "rating" in result
+            and "excerpt" in result
+            and "summary" in result
+            and "thumbnail" in result
+            and "page_count" in result
+            and "genres" in result
+            and "publisher" in result
+            and "published_date" in result
+        )
