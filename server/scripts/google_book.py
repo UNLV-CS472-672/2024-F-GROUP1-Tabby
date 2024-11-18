aeb1a86a5b9dd9f64ff8d80e42622c4a560608f9
Ederic Oytas <edericoytas@gmail.com>
2024-11-18 02:22:20 -0800

    Rewrite Google Books module; add google_book script

---
+++ server/scripts/google_book.py
@@ -0,0 +1,54 @@
+import atexit
+import logging
+from pathlib import Path
+from pprint import pprint
+import sys
+import cyclopts as cy
+from cyclopts.types import ResolvedFile
+
+from tabby_server.services import google_books
+from tabby_server.services.google_books import request_volumes_get
+from tabby_server.services.library import google_books_search
+
+
+app = cy.App()
+
+
+@app.default
+def main(
+    phrase: str = "",
+    *,
+    title: str = "",
+    author: str = "",
+    publisher: str = "",
+    subject: str = "",
+    isbn: str = "",
+    logging_file: ResolvedFile = Path("logs/google_book.log")
+) -> None:
+    """Searches for a book and prints the results
+
+    Args:
+        phrase:       Phrase parameter. Most Important. Searches everything.
+        intitle:      Title parameter.
+        inauthor:     Author parameter.
+        inpublisher:  Publisher parameter.
+        subject:      Subject parameter.
+        isbn:         ISBN parameter.
+        logging_file: File to log info.
+    """
+    if not (phrase or title or author or publisher or subject or isbn):
+        print("Please enter at least one search term.")
+        return
+
+    # Configure logging
+    f = open(str(logging_file), "w")
+    atexit.register(f.close)  # Close on program exit
+    logging.basicConfig(stream=f, level=logging.INFO)
+
+    # Make request and get results
+    result = google_books.request_volumes_get(phrase=phrase)
+    pprint(result)
+
+
+if __name__ == "__main__":
+    app()
