eb7d03067cc198ba1095e37aa1c319352f5f3d42
Ederic Oytas <edericoytas@gmail.com>
2024-11-20 18:49:05 -0800

    Update highlight_shelf; add --show-each

--- server/scripts/highlight_shelf.py
+++ server/scripts/highlight_shelf.py
@@ -12,15 +12,18 @@ from tabby_server.api.books import scan_shelf
 app = cy.App()
 
 @app.default
-def main(image_path: cy.types.ResolvedExistingFile) -> None:
+def main(image_path: cy.types.ResolvedExistingFile, *, show_each: bool = False) -> None:
     """Highlights the books on the given image of a shelf.
     
     Args:
         image_path: Path to an image of a shelf.
+        show_each: If given, shows each book individually before showing the entire picture.
     """
 
     image = cv.imread(str(image_path))
     h, w, _ = image.shape
+
+    print(f"{image.shape = }")
 
     image_processed = cv.resize(image, (640, 640))
     image_processed = image_processed / 255.0
@@ -28,7 +31,9 @@ def main(image_path: cy.types.ResolvedExistingFile) -> None:
 
     results = image_labelling.find_books(image_tensor)
 
-    pprint(results)
+    # pprint(results)
+
+    cv.namedWindow('Display', cv.WINDOW_NORMAL)
 
     display = image.copy()
     for result in results:
@@ -36,10 +41,29 @@ def main(image_path: cy.types.ResolvedExistingFile) -> None:
         x2 = int(result['box']['x2'] / 640.0 * w)
         y1 = int(result['box']['y1'] / 640.0 * h)
         y2 = int(result['box']['y2'] / 640.0 * h)
+        
+        x1 = min(max(x1, 0), w - 1)
+        x2 = min(max(x2, 0), w - 1)
+        y1 = min(max(y1, 0), h - 1)
+        y2 = min(max(y2, 0), h - 1)
+
+        # ensure x1 < x2 and y1 < y2
+        x1, x2 = min(x1, x2), max(x1, x2)
+        y1, y2 = min(y1, y2), max(y1, y2)
+
+        print(f"Subimage Corners: {(x1, y1)}, {(x2, y2)}")
 
         display = cv.rectangle(display, (x1, y1), (x2, y2), (0, 255, 0), 5)
 
-    cv.namedWindow('Display', cv.WINDOW_NORMAL)
+        if show_each:
+            subimage = image[y1:y2, x1:x2, :]
+            print(f"{subimage.shape = }")
+            if np.all(subimage.shape):  # if no dimension is 0, display it
+                cv.imshow('Subimage', subimage)
+                cv.waitKey()
+            else:
+                print("subimage has 0 length in one dimension, skipping...")
+
     cv.imshow('Display', display)
     cv.waitKey()
 
