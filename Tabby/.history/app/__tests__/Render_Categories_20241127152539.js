a4bdc11d2a7569739cf18ddb5d1efe75785dbdde
caesarx26 <caesarrobles26@gmail.com>
2024-11-27 15:31:17 -0800

    commenting out test and addding test to just render component

--- Tabby/.history/app/__tests__/Render_Categories_20241127152539.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { render } from "@testing-library/react-native";
-import Categories from "@/app/library";
-
-test("Renders Categories component", () => {
-  const { getByText } = render(<Categories />);
-  expect(getByText("Expected Text in Categories")).toBeTruthy();
-});
--- Tabby/app/__tests__/SearchBar_Test.js
+++ Tabby/app/__tests__/SearchBar_Test.js
@@ -23,47 +23,51 @@ jest.mock("expo-font", () => {
 });
 
 // tests relating to the search bar
-describe("Favorite tab tests", () => {
-  // make sure that the search bar takes input correctly on each page that it's on
-  test("Correct search bar input", () => {
-    // render favorites page
-    const favoritesPage = render(<Favorites />);
-    // get search bar
-    const searchBar = favoritesPage.getByPlaceholderText(
-      "Search by title, ISBN, or author..."
-    );
-    // change text in search bar
-    fireEvent.changeText(searchBar, "tempText");
-    // make sure search bar has proper value
-    expect(searchBar.props.value).toBe("tempText");
+// describe("Favorite tab tests", () => {
+//   // make sure that the search bar takes input correctly on each page that it's on
+//   test("Correct search bar input", () => {
+//     // render favorites page
+//     const favoritesPage = render(<Favorites />);
+//     // get search bar
+//     const searchBar = favoritesPage.getByPlaceholderText(
+//       "Search by title, ISBN, or author..."
+//     );
+//     // change text in search bar
+//     fireEvent.changeText(searchBar, "tempText");
+//     // make sure search bar has proper value
+//     expect(searchBar.props.value).toBe("tempText");
 
-    // render categories page
-    const categoriesPage = render(<Categories />);
-    // get search bar
-    const searchBar2 = categoriesPage.getByPlaceholderText(
-      "Search by category name..."
-    );
-    // change text in search bar
-    fireEvent.changeText(searchBar2, "tempText2");
-    // make sure search bar has proper value
-    expect(searchBar2.props.value).toBe("tempText2");
+//     // render categories page
+//     const categoriesPage = render(<Categories />);
+//     // get search bar
+//     const searchBar2 = categoriesPage.getByPlaceholderText(
+//       "Search by category name..."
+//     );
+//     // change text in search bar
+//     fireEvent.changeText(searchBar2, "tempText2");
+//     // make sure search bar has proper value
+//     expect(searchBar2.props.value).toBe("tempText2");
 
-    // // render categories page
-    // const categorypagePage = render(<CategoryPage />);
-    // // get search bar
-    // const searchBar3 = categorypagePage.getByPlaceholderText('Search by title, ISBN, or author...');
-    // // change text in search bar
-    // fireEvent.changeText(searchBar3, 'tempText3');
-    // // make sure search bar has proper value
-    // expect(searchBar3.props.value).toBe('tempText3');
+//     // render categories page
+//     const categorypagePage = render(<CategoryPage />);
+//     // get search bar
+//     const searchBar3 = categorypagePage.getByPlaceholderText(
+//       "Search by title, ISBN, or author..."
+//     );
+//     // change text in search bar
+//     fireEvent.changeText(searchBar3, "tempText3");
+//     // make sure search bar has proper value
+//     expect(searchBar3.props.value).toBe("tempText3");
 
-    // // render categories page
-    // const recommendationsPage = render(<Recommendations />);
-    // // get search bar
-    // const searchBar4 = recommendationsPage.getByPlaceholderText('Search by title, ISBN, or author...');
-    // // change text in search bar
-    // fireEvent.changeText(searchBar4, 'tempText4');
-    // // make sure search bar has proper value
-    // expect(searchBar4.props.value).toBe('tempText4');
-  });
-});
+//     // render categories page
+//     const recommendationsPage = render(<Recommendations />);
+//     // get search bar
+//     const searchBar4 = recommendationsPage.getByPlaceholderText(
+//       "Search by title, ISBN, or author..."
+//     );
+//     // change text in search bar
+//     fireEvent.changeText(searchBar4, "tempText4");
+//     // make sure search bar has proper value
+//     expect(searchBar4.props.value).toBe("tempText4");
+//   });
+// });
