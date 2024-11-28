7d76d9da57277de953c2abad626afb93892170ca
Josh <richardsonjosh03@gmail.com>
2024-11-28 10:46:52 -0800

    created new branch and moved all my changed from old branch to this one. There were merge conflicts with the last one and I didn't feel like doing that

--- Tabby/components/FooterNavBar.tsx
+++ Tabby/components/FooterNavBar.tsx
@@ -1,6 +1,7 @@
-import { View, Pressable, Text } from "react-native";
+import { View, Pressable, Text, Modal, FlatList, Image } from "react-native";
 import { Link, usePathname } from "expo-router";
 import React, { useState } from "react";
+import { SelectList } from 'react-native-dropdown-select-list'
 import SelectedLibrary from "@/components/navbar/selectedLibrary";
 import NotSelectedLibrary from "@/components/navbar/notSelectedLibrary";
 import SelectedExplore from "@/components/navbar/selectedExplore";
@@ -10,10 +11,16 @@ import NotSelectedFavorite from "@/components/navbar/notSelectedFavorite";
 import Settings from "@/components/navbar/settings";
 import CameraIcon from "@/components/navbar/camera";
 import CameraModal from "@/components/camera/CameraModel";
+import { Book } from '@/types/book';
+
+let tempBooks: Book[] = [];
+const tempCategories: String[] = ['Fiction', 'Non-Fiction', 'Comedy', 'Narrative', 'Mystery', 'Science Fiction', 'Thriller', 'Biography', 'Poetry'];
 
 const FooterNavBar = () => {
   // set to true to show camera modal
   const [isCameraModalVisible, setCameraModalVisible] = useState(false);
+  const [isBookSelectionModalVisible, setBookSelectionModalVisible] = useState(false);
+  const [chosenCategory, setChosenCategory] = useState('')
   const pathname = usePathname();
   const size = 40;
 
@@ -68,9 +75,70 @@ const FooterNavBar = () => {
 
       {/* Camera Modal */}
       {isCameraModalVisible && (
-        <CameraModal closeModal={() => setCameraModalVisible(false)} />
+        <CameraModal closeModal={() => setCameraModalVisible(false)}
+          onBookSelectionStart={(returnedBooks: Book[]) => {
+            tempBooks = returnedBooks;
+            setCameraModalVisible(false);
+            setBookSelectionModalVisible(true);
+          }} />
       )}
-    </View>
+      {/*TODO: - Let user choose book then click on confirm button
+               - Change logic for when books have duplicate keys(isbns)
+               - Get Categories from database
+               - Add book to database */}
+      {isBookSelectionModalVisible && (
+        <Modal animationType="slide" transparent visible>
+          <View className="flex-1 justify-center items-center  bg-opacity-50">
+            <View className="bg-white rounded-lg w-80 p-4 space-y-4 truncate">
+              <Text className="text-lg font-bold text-center">Select the correct book</Text>
+              <FlatList
+                data={tempBooks}
+                keyExtractor={(item) => item.id}
+                renderItem={({ item }) => (
+                  <Pressable
+                    onPress={() => {
+                      console.log("Book selected:", item);
+                      // TODO: add selection logic here
+                      // setBookSelectionModalVisible(false); // Close after selection
+                    }}
+                    className="p-2 border rounded-lg mb-2 h-32 flex-row"
+                  >
+                    {/* TODO: if no image make default image*/}
+                    <Image source={{ uri: item.image }} className='w-1/4 h-full' />
+                    <View className="flex-col h-32 flex-1 truncate">
+                      <Text className="text-center">{item.title}</Text>
+                      <Text className='text-center'>{item.author}</Text>
+                      <Text className='text-center truncate'>{item.summary}</Text>
+                    </View>
+                  </Pressable>
+                )}
+              />
+              <SelectList
+                setSelected={(val: string) => setChosenCategory(val)}
+                data={tempCategories}
+                search={false}
+                onSelect={() => console.log('user chose: ', chosenCategory)}
+                placeholder={'Select which category to add this book'}
+              />
+              <Pressable
+                // TODO: add selected book to database
+                onPress={() => setBookSelectionModalVisible(false)}
+                className="p-2 bg-blue-500 rounded items-center"
+              >
+                <Text className="text-white">Confirm</Text>
+              </Pressable>
+              <Pressable
+                onPress={() => setBookSelectionModalVisible(false)}
+                className="p-2 bg-red-500 rounded items-center"
+              >
+                <Text className="text-white">Cancel</Text>
+              </Pressable>
+            </View>
+          </View>
+        </Modal>
+      )
+      }
+    </View >
   );
 };
 
--- Tabby/components/camera/CameraModel.tsx
+++ Tabby/components/camera/CameraModel.tsx
@@ -1,16 +1,74 @@
-import { View, Text, Pressable, Modal, TouchableWithoutFeedback } from "react-native";
+import { View, Text, Pressable, Modal, TouchableWithoutFeedback, ActivityIndicator, FlatList } from "react-native";
 import * as ImagePicker from "expo-image-picker";
 import { useState } from "react";
-// import { useCameraPermissions } from "expo-camera";
+import { Book } from '@/types/book';
 
 interface CameraModalProps {
     closeModal: () => void;
+    onBookSelectionStart: (tempBooks: Book[]) => void;
 }
 
-const CameraModal: React.FC<CameraModalProps> = ({ closeModal }) => {
+type apiReturn = {
+    authors: string         // separated by commas
+    excerpt: string
+    isbn: string            // guaranteed to be provided, ISBN 13
+    page_count: number      // -1 if not given
+    published_date: string  // YYYY-MM-DD
+    publisher: string
+    rating: number          // -1.0 if not given
+    summary: string
+    thumbnail: string
+    title: string
+};
+
+const tempBook1: Book = {
+    id: "192083745131",
+    title: "holy hell1",
+    author: "joe mama1",
+    excerpt: "this is a sick excerpt1",
+    summary: "wow sick summary1",
+    image: "https://m.media-amazon.com/images/I/81aY1lxk+9L._AC_UF1000,1000_QL80_.jpg",
+    isFavorite: false,
+};
+
+const tempBook2: Book = {
+    id: "192083745132",
+    title: "holy hell2",
+    author: "joe mama2",
+    excerpt: "this is a sick excerpt2",
+    summary: "wow sick summary2",
+    image: "https://m.media-amazon.com/images/I/81QuEGw8VPL._AC_UF1000,1000_QL80_.jpg",
+    isFavorite: false,
+};
+
+const tempBook3: Book = {
+    id: "192083745133",
+    title: "holy hell3",
+    author: "joe mama3",
+    excerpt: "this is a sick excerpt3",
+    summary: "wow sick summary3",
+    image: "https://m.media-amazon.com/images/I/81aY1lxk+9L._AC_UF1000,1000_QL80_.jpg",
+    isFavorite: false,
+};
+
+const tempBook4: Book = {
+    id: "192083745134",
+    title: "holy hell4",
+    author: "joe mama4",
+    excerpt: "this is a sick excerpt4",
+    summary: "wow sick summary4",
+    image: "https://m.media-amazon.com/images/I/81QuEGw8VPL._AC_UF1000,1000_QL80_.jpg",
+    isFavorite: false,
+};
+
+const tempBooks: Book[] = [tempBook1, tempBook2, tempBook3, tempBook4];
+
+const CameraModal: React.FC<CameraModalProps> = ({ closeModal, onBookSelectionStart }) => {
     // const [cameraPermission, requestCameraPermission] = useCameraPermissions();
     // use to disable the buttons temporarily when clicking them to prevent multiple clicks
     const [isProcessing, setIsProcessing] = useState(false);
+    // used to determine if the user is currently choosing which book is the correct one
+    const [userChoosing, setUserChoosing] = useState(false);
 
     // Handle taking a picture by requesting permissions before taking the picture if necessary
     const handleTakePicture = async () => {
@@ -25,15 +83,14 @@ const CameraModal: React.FC<CameraModalProps> = ({ closeModal }) => {
             mediaTypes: ['images'],
             allowsEditing: true,
             quality: 1,
-            base64: true,
         });
 
-        setIsProcessing(false);
         if (!result.canceled) {
-            const resultBase64 = result.assets[0].base64; // base64 string to send to backend
-            console.log(resultBase64);
-            closeModal();
+            const returnedBooks = await uploadImage(result.assets[0].uri);
+            if (returnedBooks)
+                await userPickBook(returnedBooks);
         }
+        setIsProcessing(false);
     };
 
     // Handle picking an image from the gallery by requesting permissions before picking the image if necessary
@@ -49,23 +106,107 @@ const CameraModal: React.FC<CameraModalProps> = ({ closeModal }) => {
             mediaTypes: ['images'],
             allowsEditing: true,
             quality: 1,
-            base64: true,
         });
 
-        setIsProcessing(false);
         if (!result.canceled) {
-            const resultBase64 = result.assets[0].base64; // base64 string to send to backend
-            console.log(resultBase64);
-            closeModal();
+            const returnedBooks = await uploadImage(result.assets[0].uri);
+            if (returnedBooks)
+                await userPickBook(returnedBooks);
+        }
+        setIsProcessing(false);
+    };
+
+    // for testing activity indicator
+    async function sleep(ms: number): Promise<void> {
+        return new Promise((resolve) => setTimeout(resolve, ms));
+    }
+
+    // Opens modal for user to select correct book
+    const userPickBook = async (bookArr: Book[]) => {
+        await setUserChoosing(true);
+
+        // DONT REMOVE THIS SLEEP
+        // idk why but if you remove it then shit breaks
+        await sleep(1000);
+
+        if (onBookSelectionStart) {
+            onBookSelectionStart(bookArr);
+        }
+    }
+
+    // uploads image to scan_cover endpoint
+    const uploadImage = async (imageUri: string) => {
+        try {
+            console.log("testing koyeb image");
+
+            // convert image to blob raw data
+            const res = await fetch(imageUri);
+            const blob = await res.blob();
+
+            // fetch scan_cover
+            const response = await fetch('https://just-ulrike-tabby-app-9d270e1b.koyeb.app/books/scan_cover', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/octet-stream',
+                },
+                body: blob,
+            });
+
+            // add first 4 returned books to an array which are the books that the user can choose from
+            let returnedBooks: Book[] = [];
+            if (response.ok) {
+                console.log('success');
+                const result = await response.json();
+                // console.log("result: ", result.results[0])
+                if (result.results[0]) {
+                    const book1 = jsonToBook(result.results[0]);
+                    returnedBooks.push(book1);
+                }
+                if (result.results[1]) {
+                    const book2 = jsonToBook(result.results[1]);
+                    returnedBooks.push(book2);
+                }
+                if (result.results[2]) {
+                    const book3 = jsonToBook(result.results[2]);
+                    returnedBooks.push(book3);
+                }
+                if (result.results[3]) {
+                    const book4 = jsonToBook(result.results[3]);
+                    returnedBooks.push(book4);
+                }
+            } else {
+                console.error("error uploading image: ", response.status);
+                const errorText = await response.text();
+                console.error("Error details: ", errorText);
+            }
+            return returnedBooks;
+        } catch (error) {
+            console.error("Error uploading image:", error);
         }
     };
 
+    // returns a Book object from json given by google books
+    const jsonToBook = (bookjson: apiReturn) => {
+        const returnBook: Book = {
+            id: bookjson.isbn,
+            title: bookjson.title,
+            author: bookjson.authors,
+            excerpt: bookjson.excerpt,
+            summary: bookjson.summary,
+            image: bookjson.thumbnail,
+            // rating: bookjson.rating, (cant do rating like this)
+            isFavorite: false,
+        }
+        return returnBook;
+    }
+
     return (
-        <Modal animationType="fade" transparent visible>
+        <Modal animationType="fade" transparent visible={!userChoosing}>
             <TouchableWithoutFeedback onPress={closeModal}>
                 <View className="flex-1 justify-center items-center">
                     <TouchableWithoutFeedback>
                         <View className="bg-white rounded-lg w-64 p-4 space-y-4">
+                            {isProcessing && <ActivityIndicator size='large' color='#0000ff' />}
                             <Pressable
                                 onPress={handleTakePicture}
                                 disabled={isProcessing}
